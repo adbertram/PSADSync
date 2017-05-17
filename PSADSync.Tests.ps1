82673c3f88cfd3d51db4e5a16db12f9ffeeb7f67
Adam Bertram <adbertram@gmail.com>
2017-05-17 09:55:55 -0500

    Updated CSV header test

--- PSADSync.Tests.ps1
+++ PSADSync.Tests.ps1
@@ -109,8 +109,8 @@ InModuleScope $ThisModuleName {
 			mock 'Get-Content' {
 				@(
 					'"Header1","Header2","Header3"'
-					'Value1,Value2,Value3'
-					'Value4,Value5,Value6'
+					'"Value1","Value2","Value3"'
+					'"Value4","Value5","Value6"'
 				)
 			}
 		#endregion
