565e1500a6bda4eb9cf141890a193d8926b9c920
Adam Bertram <adbertram@gmail.com>
2017-05-20 13:37:31 -0500

    Appveyor testing

--- buildscripts/build.ps1
+++ buildscripts/build.ps1
@@ -1,13 +1,14 @@
 try {
 	$ErrorActionPreference = 'Stop'
 
-	Write-Host (ls env:\ | Out-String)
 	## Update module version in manifest
 	$manifestFilePath = "$env:APPVEYOR_BUILD_FOLDER\PSADSync.psd1"
 	((Get-Content -Path $manifestFilePath) -replace "ModuleVersion = '.*'","ModuleVersion = '$env:APPVEYOR_BUILD_VERSION'") | Set-Content -Path $manifestFilePath
 
 	## Export only certain functions
 	((Get-Content -Path $manifestFilePath) -replace "FunctionsToExport = '\*'","FunctionsToExport = 'Invoke-AdSync'") | Set-Content -Path $manifestFilePath
+
+	Write-Host (Get-Content -Path $manifestFilePath -Raw)
 
 	## Publish module to PowerShell Gallery
 	Publish-Module -Path $env:APPVEYOR_BUILD_FOLDER -NuGetApiKey $env:nuget_apikey
