6364e551d123bcc1fefa9094b78a1da07084c34d
Adam Bertram <adbertram@gmail.com>
2017-05-20 15:33:36 -0500

    appveyor publish updates

--- buildscripts/publish.ps1
+++ buildscripts/publish.ps1
@@ -1,8 +1,11 @@
 $ErrorActionPreference = 'Stop'
 
+## To silence the progress bar for Publish-Module
+$ProgressPreference = 'SilentlyContinue'
+
 try {
-	Install-PackageProvider -Name NuGet -MinimumVersion '2.8.5.208' -Force -Verbose
-	
+	$null = Install-PackageProvider -Name NuGet -MinimumVersion '2.8.5.208' -Force -Verbose
+
 	Import-Module -Name PowerShellGet
 
 	## Don't upload the build scripts to PowerShell Gallery
@@ -11,7 +14,7 @@ try {
 	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Filter '*.ps*' | Copy-Item -Destination $moduleFolderPath
 
 	## Publish module to PowerShell Gallery
-	Publish-Module -Path $moduleFolderPath -NuGetApiKey $env:nuget_apikey -Confirm:$false
+	Publish-Module -Path $moduleFolderPath -NuGetApiKey $env:nuget_apikey -Confirm:$false -Verbose
 
 } catch {
 	$host.SetShouldExit($LastExitCode)
