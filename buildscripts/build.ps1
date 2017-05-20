40922a307aca00a90440972fec7db526c1f56e7f
Adam Bertram <adbertram@gmail.com>
2017-05-20 14:18:38 -0500

    appveyor updates

--- buildscripts/build.ps1
+++ buildscripts/build.ps1
@@ -13,8 +13,13 @@ try {
 	}
 	Update-ModuleManifest @updateParams
 
+	## Don't upload the build scripts to PowerShell Gallery
+	$moduleFolderPath = "$env:APPVEYOR_BUILD_FOLDER\Module"
+	$null = mkdir $moduleFolderPath
+	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Filter '*.ps*' | where { -not $_.PSIsContainer } | Copy-Item -Destination $moduleFolderPath
+
 	## Publish module to PowerShell Gallery
-	Publish-Module -Path $env:APPVEYOR_BUILD_FOLDER -NuGetApiKey $env:nuget_apikey -Confirm:$false -Verbose
+	Publish-Module -Path $moduleFolderPath -NuGetApiKey $env:nuget_apikey -Confirm:$false
 
 } catch {
 	$host.SetShouldExit($LastExitCode)
