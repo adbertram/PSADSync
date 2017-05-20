89aba22ac959b97cb33bd0c48e92fffaa5fdd446
Adam Bertram <adbertram@gmail.com>
2017-05-20 15:09:30 -0500

    appveyor publish updates

--- buildscripts/test.ps1
+++ buildscripts/test.ps1
@@ -1,8 +1,13 @@
-$ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
+$ErrorActionPreference = 'Stop'
+try {
+	$ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
 
-$testResultsFilePath = "$ProjectRoot\TestResults.xml"
+	$testResultsFilePath = "$ProjectRoot\TestResults.xml"
 
-Invoke-Pester -Path "$ProjectRoot\PSADSync.Tests.ps1" -OutputFormat NUnitXml -OutputFile $testResultsFilePath -EnableExit
+	Invoke-Pester -Path "$ProjectRoot\PSADSync.Tests.ps1" -OutputFormat NUnitXml -OutputFile $testResultsFilePath -EnableExit
 
-$Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
-(New-Object 'System.Net.WebClient').UploadFile( $Address, $testResultsFilePath )
+	$Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+	(New-Object 'System.Net.WebClient').UploadFile( $Address, $testResultsFilePath )
+} catch {
+	$host.SetShouldExit($LastExitCode)
+}
