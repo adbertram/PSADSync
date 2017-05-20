6f341fa5fe81a49efa001ac121c5054d03edf1da
Adam Bertram <adbertram@gmail.com>
2017-05-20 16:21:42 -0500

    build update

--- buildscripts/build.ps1
+++ buildscripts/build.ps1
@@ -2,6 +2,9 @@ $ErrorActionPreference = 'Stop'
 
 try {
 
+	$manifestFilePath = "$env:APPVEYOR_BUILD_FOLDER\PSAdSync.psd1"
+	$manifestContent = Get-Content -Path $manifestFilePath -Raw
+	
 	## Update the module version based on the build version and limit exported functions
 	$replacements = @{
 		"ModuleVersion = '.*'" = "ModuleVersion = '$env:APPVEYOR_BUILD_VERSION'"
