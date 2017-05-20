fcb8aa28d5cf92ad989fce52f329dc116f5f888d
Adam Bertram <adbertram@gmail.com>
2017-05-20 09:52:10 -0500

    Appveyor build update

--- buildscripts/build.ps1
+++ buildscripts/build.ps1
@@ -8,6 +8,7 @@ try {
 	## Export only certain functions
 	((Get-Content -Path $manifestFilePath) -replace "FunctionsToExport = '\*'","FunctionsToExport = 'Invoke-AdSync'") | Set-Content -Path $manifestFilePath
 
+	## Publish module to PowerShell Gallery
 	Publish-Module -Path $env:APPVEYOR_BUILD_FOLDER -NuGetApiKey $env:nuget_apikey
 
 } catch {
--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -1,5 +1,4 @@
 Write-Host 'Installing necessary PowerShell modules...'
 $null = Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
 $null = Import-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
-Install-Module AzureRM -verbose -Force -Confirm:$false -AllowClobber
 Install-Module Pester -verbose -Force -Confirm:$false
