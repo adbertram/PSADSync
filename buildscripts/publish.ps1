cab20fa61d6c59d4da372650da635628e2d55514
Adam Bertram <adbertram@gmail.com>
2017-05-22 15:26:53 -0500

    Appveyor publish tweak

--- buildscripts/publish.ps1
+++ buildscripts/publish.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
 try {
-	## Don't upload the build scripts to PowerShell Gallery
+	## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
 	$moduleFolderPath = "$env:APPVEYOR_BUILD_FOLDER\PSADSync"
 	$null = mkdir $moduleFolderPath
-	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Filter '*.ps*' | Copy-Item -Destination $moduleFolderPath
+	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER | where { $_.Name -notmatch 'buildscripts|appveyor\.yml'} | Copy-Item -Destination $moduleFolderPath
 
 	## Publish module to PowerShell Gallery
 	$publishParams = @{
