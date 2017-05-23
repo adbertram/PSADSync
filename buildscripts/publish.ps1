171cf72109ef035341269c61b857ed4f62c363db
Adam Bertram <adbertram@gmail.com>
2017-05-23 10:21:42 -0500

    publish troubleshooting

--- buildscripts/publish.ps1
+++ buildscripts/publish.ps1
@@ -1,22 +1,32 @@
 $ErrorActionPreference = 'Stop'
 
-try {
-	## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
-	$moduleFolderPath = "$env:APPVEYOR_BUILD_FOLDER\PSADSync"
-	$null = mkdir $moduleFolderPath
-	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER | where { $_.Name -notmatch 'buildscripts|appveyor\.yml'} | Copy-Item -Destination $moduleFolderPath
+try
+{
+    ## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
+    $moduleFolderPath = "$env:APPVEYOR_BUILD_FOLDER\PSADSync"
+    $null = mkdir $moduleFolderPath
+    $excludeFromPublish = @(
+        'PSADSync\\buildscripts'
+        'PSADSync\\appveyor\.yml'
+        'PSADSync\\\.git'
+        'PSADSync\\README\.md'
+    )
+    $exclude = $excludeFromPublish -join '|'
+    $packageContents = Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | where { $_.FullName -notmatch $exclude } 
+	$packageContents | Copy-Item -Destination {Join-Path -Path $moduleFolderPath -ChildPath $_.FullName.Substring($env:APPVEYOR_BUILD_FOLDER.length)}
 
-	## Publish module to PowerShell Gallery
-	$publishParams = @{
-		Path = $moduleFolderPath
-		NuGetApiKey = $env:nuget_apikey
-		Repository = 'PSGallery'
-		Force = $true
-		Confirm = $false
-	}
-	Publish-Module @publishParams
+    ## Publish module to PowerShell Gallery
+    $publishParams = @{
+        Path        = $moduleFolderPath
+        NuGetApiKey = $env:nuget_apikey
+        Repository  = 'PSGallery'
+        Force       = $true
+        Confirm     = $false
+    }
+    Publish-Module @publishParams
 
-} catch {
-	Write-Error -Message $_.Exception.Message
-	$host.SetShouldExit($LastExitCode)
+} catch
+{
+    Write-Error -Message $_.Exception.Message
+    $host.SetShouldExit($LastExitCode)
 }
