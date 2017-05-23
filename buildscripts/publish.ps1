9457a28db067834ee1ca35b6d5ab1b38fce00e7f
Adam Bertram <adbertram@gmail.com>
2017-05-23 17:33:12 -0500

    Better publishing

--- buildscripts/publish.ps1
+++ buildscripts/publish.ps1
@@ -1,32 +1,34 @@
 $ErrorActionPreference = 'Stop'
 
-try
-{
-    ## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
-    $moduleFolderPath = "$env:APPVEYOR_BUILD_FOLDER\PSADSync"
-    $null = mkdir $moduleFolderPath
-    $excludeFromPublish = @(
-        'PSADSync\\buildscripts'
-        'PSADSync\\appveyor\.yml'
-        'PSADSync\\\.git'
-        'PSADSync\\README\.md'
-    )
-    $exclude = $excludeFromPublish -join '|'
-    $packageContents = Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | where { $_.FullName -notmatch $exclude } 
-	$packageContents | Copy-Item -Destination {Join-Path -Path $moduleFolderPath -ChildPath $_.FullName.Substring($env:APPVEYOR_BUILD_FOLDER.length)}
+try {
+	## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
+	$tempmoduleFolderPath = "$env:Temp\PSAdSync"
+	$null = mkdir $tempmoduleFolderPath
 
-    ## Publish module to PowerShell Gallery
-    $publishParams = @{
-        Path        = $moduleFolderPath
-        NuGetApiKey = $env:nuget_apikey
-        Repository  = 'PSGallery'
-        Force       = $true
-        Confirm     = $false
-    }
-    Publish-Module @publishParams
+	## Move all of the files/folders to exclude out of the main folder
+	$excludeFromPublish = @(
+		'PSAdSync\\buildscripts'
+		'PSAdSync\\appveyor\.yml'
+		'PSAdSync\\\.git'
+		'PSAdSync\\README\.md'
+	)
+	$exclude = $excludeFromPublish -join '|'
+	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | where { $_.FullName -match $exclude } | Move-Item -Destination $env:temp
 
-} catch
-{
-    Write-Error -Message $_.Exception.Message
-    $host.SetShouldExit($LastExitCode)
+	## Copy only the package contents to the module folder
+	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | Copy-Item -Destination $tempmoduleFolderPath
+
+	## Publish module to PowerShell Gallery
+	$publishParams = @{
+		Path = $tempmoduleFolderPath
+		NuGetApiKey = $env:nuget_apikey
+		Repository = 'PSGallery'
+		Force = $true
+		Confirm = $false
+	}
+	Publish-Module @publishParams
+
+} catch {
+	Write-Error -Message $_.Exception.Message
+	$host.SetShouldExit($LastExitCode)
 }
