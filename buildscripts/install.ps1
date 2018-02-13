f5e7becc50ce6dfe15c403ea9dfabc6e39c1b04d
Adam Bertram <adbertram@gmail.com>
2018-02-13 11:18:57 -0600

    Build script updates

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -15,7 +15,9 @@ try {
 	foreach ($m in $requiredModules) {
 		Write-Host "Installing [$($m)] module..."
 		Install-Module -Name $m -Force -Confirm:$false
+		Write-Host "Removing [$($m)] module from current session..."
 		Remove-Module -Name $m -Force -ErrorAction Ignore
+		Write-Host "Importing [$($m)] module into current session..."
 		Import-Module -Name $m
 	}
 
