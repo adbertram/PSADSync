c8228cde51c912e294c470bb00c5c382480030aa
Adam Bertram <adbertram@gmail.com>
2018-02-13 11:11:14 -0600

    Build script updates

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -11,7 +11,7 @@ try {
 	$null = Install-PackageProvider @provParams
 	$null = Import-PackageProvider @provParams
 
-	$requiredModules = @('Pester', 'PowerShellGet', 'PSScriptAnalyzer', 'PackageManagement')
+	$requiredModules = @('Pester', 'PackageManagement', 'PowerShellGet', 'PSScriptAnalyzer')
 	foreach ($m in $requiredModules) {
 		Write-Host "Installing [$($m)] module..."
 		Install-Module -Name $m -Force -Confirm:$false
